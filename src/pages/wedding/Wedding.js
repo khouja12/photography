cd0b7dc6569b2c51744934ca49a4398ab03c78a4
khouja12 <63063305+khouja12@users.noreply.github.com>
2024-03-23 06:46:01 +0100

    yosra

--- src/pages/wedding/Wedding.js
+++ src/pages/wedding/Wedding.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the carousel CSS
+import { Carousel } from "react-responsive-carousel";
+import img1 from "../../img/Wedding image/img1.JPG";
+import img2 from "../../img/Wedding image/img2.JPG";
+import img3 from "../../img/Wedding image/img3.JPG";
+import img4 from "../../img/Wedding image/img4.JPG";
+import img5 from "../../img/Wedding image/img5.JPG";
+import "./Wedding.css";
 
-const Wedding = () => {
+const CarousselUI = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const image = [img2, img3, img4];
+  const handleSelect = (index) => {
+    setSelectedIndex(index);
+  };
+
   return (
     <div>
-      
+      <container className="caroussel-container">
+        <div className="ImageWedding">
+          <Carousel
+            className="carousselWedding"
+            selectedItem={selectedIndex}
+            onChange={handleSelect}
+            showStatus={false} // Hide status indicators
+            showThumbs={true} // Hide thumbnail images
+            infiniteLoop={true} // Enable infinite loop
+            autoPlay={true} // Enable autoplay
+            interval={5000} // Set autoplay interval to 5 seconds
+          >
+            {image.map((image, index) => (
+              <div key={index}>
+                <img src={image} alt={`${index + 1}`} />
+              </div>
+            ))}
+          </Carousel>
+          <div className="sideImageWedding" >
+            <img src={img1} />
+            <img src={img5} />
+          </div>
+        </div>
+      </container>
     </div>
-  )
-}
+  );
+};
 
-export default Wedding
+export default CarousselUI;
